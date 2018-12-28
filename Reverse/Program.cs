c9209dd0193c697af673c2f03d6f73e6fedb11de
Djordje Dasic <djole87dasic@gmail.com>
2018-12-28 07:58:20 +0100

    first repo

---
+++ Reverse/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Reversa game");
+
+            Console.Write("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Please enter your city of born: ");
+            string city = Console.ReadLine();
+            
+            //DisplayResult(ReverseArray(firstName), ReverseArray(lastName), ReverseArray(city));
+            DisplayResult(ReverseArray(firstName) + " " + ReverseArray(lastName) + " " + ReverseArray(city));
+            
+            Console.ReadLine();
+        }
+       private static string ReverseArray(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            return string.Concat(messageArray);
+            
+        }
+        /*private static void DisplayResult(string reversedFirstName, string reversedLastName, string reversedCity)
+        {
+            Console.Write(string.Format("{0} {1} {2}",
+                reversedFirstName,
+                reversedLastName,
+                reversedCity));
+        }*/
+        private static void DisplayResult(string message)
+        {
+            Console.Write(message);
+        }
+
+    }
+}
